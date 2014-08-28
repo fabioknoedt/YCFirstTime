d30c65393d5c0a72a268a0784eec144f0630d61c
Fabio Knoedt <fabio.knoedt@datedicted.de>
2014-08-28 11:20:13 +0200

    Initial Commit

---
+++ FKFirstTime/FKFirstTime.h
@@ -0,0 +1,13 @@
+//
+//  FKFirstTime.h
+//  FKFirstTime
+//
+//  Created by Fabio Knoedt on 28/08/14.
+//  Copyright (c) 2014 Fabio Knoedt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FKFirstTime : NSObject
+
+@end
---
+++ FKFirstTime/FKFirstTime.m
@@ -0,0 +1,13 @@
+//
+//  FKFirstTime.m
+//  FKFirstTime
+//
+//  Created by Fabio Knoedt on 28/08/14.
+//  Copyright (c) 2014 Fabio Knoedt. All rights reserved.
+//
+
+#import "FKFirstTime.h"
+
+@implementation FKFirstTime
+
+@end
